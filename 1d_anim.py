de2b0520979389bcf14c41e294ded4016954cf9b
henri <henri.rapeli@gmail.com>
2023-06-17 16:33:11 +0300

    Updated the creation of the animation

--- 1d_anim.py
+++ 1d_anim.py
@@ -1,6 +1,6 @@
 """
-Animation of one dimensional (with regards to space) waves that have a wave
-function of form f(x, t) = A*cos(k*x - w*t + delta)
+Animation of one dimensional (with regards to the spatial dimensions) waves
+that have a wave function of form f(x, t) = A*cos(k*x - w*t + delta)
 """
 import functools
 
@@ -41,33 +41,19 @@ def wavefun(x: np.ndarray, t: np.ndarray, amp: int | float, k: int | float,
     return amp * np.cos(k * x - w * t + delta)
 
 
-def _init_anim(lines: list[plt.Line2D]) -> list[plt.Line2D]:
-    """
-    Initialises the plots (basically setting the coordinates to empty)
-    :param lines:
-    :return:
-    """
-    for i, line in enumerate(lines):
-        line.set_data([], [])
-    return lines
-
-
-def _update_anim(i: int, lines: list[plt.Line2D], x: np.ndarray,
+def _update_anim(n: int, lines: list[plt.Line2D], x: np.ndarray,
                  data: np.ndarray) -> list[plt.Line2D]:
     """
     Updates the plots
-    :param i:
+    :param n:
     :param lines:
     :param x:
     :param data:
     :return:
     """
-    left_y = data[0, i, :]
-    right_y = data[1, i, :]
-    summa_y = data[2, i, :]
-    lines[0].set_data(x, left_y)
-    lines[1].set_data(x, right_y)
-    lines[2].set_data(x, summa_y)
+    for i, line in enumerate(lines):
+        y = data[i, n, :]
+        line.set_data(x, y)
     return lines
 
 
@@ -87,19 +73,18 @@ def animate(*funcs: functools.partial,  x: np.ndarray, t: np.ndarray) -> None:
     data[-1, :, :] = np.sum(data, axis=0)
     fig, ax = plt.subplots(figsize=(10, 6))
     ax.grid()
-    colors = ['b', 'g', 'r', 'y']
-    alpha = .75
+    colors = ['b', 'g', 'r', 'y', 'm']
+    alpha = .5
     # Lines for the individual waves
-    lines = [ax.plot([], [], c=colors[i % n], ls='--', alpha=alpha)[0]
+    lines = [ax.plot([], [], c=colors[i % len(colors)], ls='--', alpha=alpha)[0]
              for i in range(n)]
-    # Line for the sum wave
+    # Line for the sum wave separately with different alpha and style
     lines.append(ax.plot([], [], c='k')[0])
     ax.set_xlim(np.min(x), np.max(x))
     ax.set_ylim(np.min(data[-1]), np.max(data[-1]))
-    # lines = _init_anim(lines)
     anim = FuncAnimation(fig=fig, func=_update_anim, frames=len(t),
                          fargs=(lines, x, data), blit=True)
-    anim.save(filename='yeet.gif', writer='pillow', fps=60, dpi=100)
+    anim.save(filename='anim.gif', writer='pillow', fps=60, dpi=100)
 
 
 def main():
@@ -111,9 +96,13 @@ def main():
     delta = 0
     x = np.arange(0, 2 * np.pi, .01)
     t = np.arange(0, 2 * np.pi, .005)
-    wave1 = partial(wavefun, amp=amp, k=k, w=w, delta=delta)
-    wave2 = partial(wavefun, amp=amp, k=k, w=-w, delta=delta)
-    animate(wave1, wave2, x=x, t=t)
+    waves = []
+    for i in range(8):
+        n = 2 ** i
+        sign = 1 if i % 2 == 0 else -1
+        wave = partial(wavefun, amp=amp/n, k=k*n, w=n*w*sign, delta=delta)
+        waves.append(wave)
+    animate(*waves, x=x, t=t)
 
 
 if __name__ == '__main__':
